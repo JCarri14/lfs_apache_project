7b17c366b7e1c50999168a0889c848a2b5647042
JCarri14 <j.carrion@students.salle.url.edu>
2021-06-21 20:19:05 +0200

    feat: add music player view

---
+++ js/utils/iptables-utils.js
@@ -0,0 +1,43 @@
+import { request } from "./api-utils.js";
+import { getCurrentUser } from "./state-utils.js";
+
+const ipTablesURL = "http://localhost/scripts/iptables-list.sh";
+//const createUserURL = "http://localhost/scripts/user-create.sh";
+//const deleteUserURL = "http://localhost/scripts/user-delete.sh";
+
+export async function fetchIptables() {
+    const res = await request({
+        url: ipTablesURL,
+        method: "GET",
+    });
+
+    if (res.isSuccessful) {
+        return res.data.rules;
+    } else {
+        return [];
+    }
+}
+
+
+
+export async function createRule(values) { 
+    console.log(values);
+}
+
+export async function removeRule(name) {
+    const creator = await getCurrentUser();
+    console.log(`creator:${creator}`);
+    const postData = `creator:${creator};user:${name};`;
+
+    const res = await request({
+        url: deleteUserURL,
+        method: "POST",
+        headers: {
+            'Accept': 'text/plain',
+            'Content-Type': 'text/plain',
+        },
+        body: postData
+    });
+
+    return res;
+}
